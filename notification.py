3a473074c38cf5b6bf20e892ffb3b9c188b38425
Abishek Bhatta <abishekbhatta003@gmail.com>
2024-09-08 22:48:06 -0500

    Feature Update
    Added a feature where the bot shows the current price in the notification.

--- notification.py
+++ notification.py
@@ -1,13 +1,13 @@
 #this moudle contains desktop_notification function which pops up notifcation to desktop on being called
 
 from plyer import notification
-
+from scraper import scrape_price
 
 def desktop_notification():
         notification.notify(
                 app_name="Bot",
                 app_icon= "media/icons8-alert-100.ico",
-                message="MacBook Pro M3 Pro chip price has been dropped down. Check at Amazon.",
+                message= f"MacBook Pro M3 Pro chip price has been dropped down. The current price is ${scrape_price()}.",
                 title="Price Drop Alert",
                 timeout = 5
         )
